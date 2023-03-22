43e1e68cfdc41eb3641f24b5f409919d596f8905
Richard Antunis <richardantunis4@gmail.com>
2023-03-22 17:30:10 -0300

    adding share form and fixing bugs

--- js/main.js
+++ js/main.js
@@ -1,11 +1,14 @@
+
+let moreButton = document.querySelector('#more')
 let products = document.querySelector('.products-section')
+let url= 'https://frontend-intern-challenge-api.iurykrieger.vercel.app/products?page=1'
 let name =''
 let img = ''
 let desc =''
 let instalCount = ''
 let instalValue = ''
 let oldPrice =''
-let price =''
+let price = ''
 function popularCampos(response){
     for(let i =0; i < response.products.length; i++){
         name = response.products[i].name
@@ -16,29 +19,41 @@ function popularCampos(response){
         instalCount = response.products[i].installments.count
         instalValue = response.products[i].installments.value
         products.innerHTML += ` <div class="products">
-        
-            <div class="divProductImg">
-            <img src="${img}">
-            </div>
-            <label class="productName">${name}</label>
-            <p class="productDescription">${desc}</p>
-            <span class="oldPrice">De: R$${oldPrice}</span>
-            <h3 class="price">Por: R$${price}</h3>
-            <span class="installments">ou ${instalCount}x de R$${instalValue}</span>
-            <button class="buyBtn"> Comprar</button>
-          </div>
-        ` 
+                        <div class="divProductImg">
+                        <img src="${img}">
+                        </div>
+                        <label class="productName">${name}</label>
+                        <p class="productDescription">${desc}</p>
+                        <span class="oldPrice">De: R$${oldPrice}</span>
+                        <h3 class="price">Por: R$${price}</h3>
+                        <span class="installments">ou ${instalCount}x de R$${instalValue}</span>
+                        <button class="buyBtn"> Comprar</button>
+                    </div>
+                ` 
     }
-    products.innerHTML += `<button class="moreBtn">Ainda mais produtos aqui!</button> ` 
-    
-}    
-function error (){
+}
+   
+        
+function error(){
     alert('Erro')
 }
-const query = () =>{
-    fetch('https://frontend-intern-challenge-api.iurykrieger.vercel.app/products?')
-    .then(response => response.json()
-    .then(popularCampos))
-    //.catch(error)
+function query(){
+    fetch(url)
+    .then(response => response.json())
+    .then(response => {
+        popularCampos(response)
+            let dados = response.nextPage
+            url = `https://${dados}`
+    })
+    .catch(error)
 }
 query()
+
+moreButton.addEventListener('click', e =>{
+    e.preventDefault()
+
+    query()
+})
+        
+
+
