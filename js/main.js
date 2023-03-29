c517dee00355edec87bed1b214fe8e0fab644c92
Richard Antunis <richardantunis4@gmail.com>
2023-03-29 17:45:17 -0300

    adding  mobile media querie

--- js/main.js
+++ js/main.js
@@ -24,28 +24,62 @@ const emailShare = document.querySelector('#emailShare')
 
 //api request
 function popularCampos(response){
-    for(let i =0; i < response.products.length; i++){
-        name = response.products[i].name
-        img = response.products[i].image
-        desc = response.products[i].description
-        oldPrice = response.products[i].oldPrice
-        price = response.products[i].price
-        instalCount = response.products[i].installments.count
-        instalValue = response.products[i].installments.value
-        products.innerHTML += ` <div class="products">
-                        <div class="divProductImg">
-                        <img src="${img}">
-                        </div>
+    if(window.matchMedia("(min-width: 421px)").matches){
+
+        for(let i =0; i < response.products.length; i++){
+            name = response.products[i].name
+            img = response.products[i].image
+            desc = response.products[i].description
+            oldPrice = response.products[i].oldPrice
+            price = response.products[i].price
+            instalCount = response.products[i].installments.count
+            instalValue = response.products[i].installments.value
+            products.innerHTML += ` <div class="products">
+                    <div class="divProductImg">
+                    <img src="${img}">
+                    </div>
+                    <div>
                         <label class="productName">${name}</label>
                         <p class="productDescription">${desc}</p>
                         <span class="oldPrice">De: R$${oldPrice}</span>
                         <h3 class="price">Por: R$${price}</h3>
                         <span class="installments">ou ${instalCount}x de R$${instalValue}</span>
                         <button class="buyBtn"> Comprar</button>
-                    </div>
-                ` 
+                    </div>    
+                </div>
+            ` 
+        }
     }
-}      
+    else{
+        for(let i =0; i < 4; i++){
+            name = response.products[i].name
+            img = response.products[i].image
+            desc = response.products[i].description
+            oldPrice = response.products[i].oldPrice
+            price = response.products[i].price
+            instalCount = response.products[i].installments.count
+            instalValue = response.products[i].installments.value
+            products.innerHTML += ` <div class="products">
+                    <div class="divProductImg">
+                    <img src="${img}">
+                    </div>
+                    <div>
+                        <label class="productName">${name}</label>
+                        <p class="productDescription">${desc}</p>
+                        <span class="oldPrice">De: R$${oldPrice}</span>
+                        <h3 class="price">Por: R$${price}</h3>
+                        <span class="installments">ou ${instalCount}x de R$${instalValue}</span>
+                        <button class="buyBtn"> Comprar</button>
+                    </div>    
+                </div>
+            ` 
+        }
+    } 
+  
+    
+  
+}  
+
 function error(){
     alert('Erro')
 }
@@ -141,5 +175,3 @@ function checkInputsShare(){
         succesValidation(emailShare)
     }
 }  
-
-
